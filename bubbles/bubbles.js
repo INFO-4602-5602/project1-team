e77431db25d0d55d2bf20c5d53d0994ddab8ca8c
Samantha Molnar <samantha.molnar@colorado.edu>
2017-04-12 10:09:29 -0600

    moved datafile up the tree

--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -12,7 +12,7 @@ var pack = d3.pack()
     .size([diameter - margin, diameter - margin])
     .padding(2);
 
-d3.json("/data/result.json", function(error, root) {
+d3.json("result.json", function(error, root) {
   if (error) throw error;
 
   root = d3.hierarchy(root)
